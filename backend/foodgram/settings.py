ebc48c05b52e165da4a2f2c98c04b73ad86d3923
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-06-03 19:11:24 +0300

    Git actions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -109,11 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'staticf/'
+STATIC_URL = 'staticf/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticf')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-# MEDIA_URL = 'media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
