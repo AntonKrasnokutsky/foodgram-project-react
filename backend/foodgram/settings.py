0ef9c5538f5b87ce59910f0479dfdcaecb7c2b08
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-06-03 12:56:02 +0300

    Git actions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
