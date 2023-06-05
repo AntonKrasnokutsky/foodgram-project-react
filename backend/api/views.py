f7780905a721b2c55472ae7c570168dc86c0ac8b
Anton Krasnokutsky <anton.krasnokutsky@gmail.com>
2023-06-05 11:30:53 +0300

    Git actions

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
-# from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
@@ -38,7 +37,7 @@ class IngredientsViewSet(
     serializer_class = IngredientsSerializer
     pagination_class = None
     filter_backends = [filters.SearchFilter, ]
-    search_fields = ['name', ]
+    search_fields = ['$name', ]
 
 
 class TagsViewSet(
