905ed39a15e05d9abe0d7f07800d404a59d088fd
Anton Krasnokutsky <anton.krasnokutsky@gmail.com>
2023-06-05 12:51:23 +0300

    Git actions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'rest_framework_word_filter',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
