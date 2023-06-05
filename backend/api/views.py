fd0e4ef3e5fd8369ea00cbcdd0a9dea539fb0120
Anton Krasnokutsky <anton.krasnokutsky@gmail.com>
2023-06-05 14:07:00 +0300

    Git actions

--- backend/api/views.py
+++ backend/api/views.py
@@ -40,7 +40,7 @@ class IngredientsViewSet(
     # filter_backends = (DjangoFilterBackend, )
     # filterset_fields = ('$name',)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('$name',)
+    search_fields = ('@name',)
 
 
 class TagsViewSet(
